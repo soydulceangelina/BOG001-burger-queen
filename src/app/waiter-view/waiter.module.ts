dfd06b4558094bc692b510a6e0c905acd3af9262
Dulce Mercado <soydulceangelina@gmail.com>
2020-11-03 23:05:08 -0500

    refactorizar archivos, optimizar uso de componentes

--- src/app/waiter-view/waiter.module.ts
+++ src/app/waiter-view/waiter.module.ts
@@ -5,9 +5,7 @@ import { FormsModule } from '@angular/forms';
 import {SharedModule} from '../shared/shared.module';
 
 import { HeaderNavComponent } from './components/header-nav/header-nav.component';
-import { BreakfastProductsComponent } from './components/breakfast-products/breakfast-products.component';
 import { LunchProductsComponent } from './components/lunch-products/lunch-products.component';
-import { DrinksProductsComponent } from './components/drinks-products/drinks-products.component';
 import { InputSectionComponent } from './components/input-section/input-section.component';
 import {WaiterViewComponent} from './components/waiter/waiter-view.component';
 import {TotalListComponent} from './components/total-list/total-list.component';
@@ -16,9 +14,7 @@ import {WaiterRoutingModule} from './waiter-routing.module';
 
 @NgModule({
     declarations: [
-        BreakfastProductsComponent,
         LunchProductsComponent,
-        DrinksProductsComponent,
         HeaderNavComponent,
         InputSectionComponent,
         WaiterViewComponent,
