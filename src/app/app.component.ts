14537f52c38d7ae1079dc53b3f354c3b3266847b
Dulce Mercado <soydulceangelina@gmail.com>
2020-11-10 11:19:16 -0500

    Added changes to url

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { AngularFireMessaging } from '@angular/fire/messaging';
 import { AngularFirestore } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 
@@ -9,11 +11,28 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent {
 
-   items: Observable<any[]>;
+  //  items: Observable<any[]>;
 
-  constructor(firestore: AngularFirestore) {
-    this.items = firestore.collection('items').valueChanges();
+  // constructor(firestore: AngularFirestore) {
+  //   this.items = firestore.collection('items').valueChanges();
+  // }
+
+  constructor(
+    private swUpdate: SwUpdate,
+    private messaging: AngularFireMessaging
+  ){}
+
+  ngOnInit() {
+    this.updatePWA();
+    // this.requestPermission();
+    // this.listenNotifications();
   }
 
+  updatePWA() {
+    this.swUpdate.available.subscribe(value => {
+      window.location.reload();
+    });
+  }
+  
   title = 'burgerQueen';
 }
