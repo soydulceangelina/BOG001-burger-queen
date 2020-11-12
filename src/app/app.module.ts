e3d085a8116ec336040785d0bc7223b7aefe13ef
Dulce Mercado <soydulceangelina@gmail.com>
2020-11-12 12:53:40 -0500

    cambios en app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
+import {AngularFireModule} from '@angular/fire'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -27,7 +29,9 @@ import { environment } from '../environments/environment';
     CoreModule,
     HttpClientModule,
     [SweetAlert2Module.forRoot(),
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })]
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })],
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule.enablePersistence()
   ],
   providers: [],
   bootstrap: [AppComponent]
